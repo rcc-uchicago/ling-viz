53a2b8f54ab6dcf187f20771cb3ef5e29f28ef36
Simon Jacobs <sdjacobs@uchicago.edu>
2014-07-29 15:13:41 -0500

    3d initial commit

--- webui/graphutils.js
+++ webui/graphutils.js
@@ -1,3 +1,5 @@
+d3.ns.prefix.x3da="http://www.web3d.org/specifications/x3d-namespace"
+
 /* XML Parsing notes --
    D3 edges (as read from JSON) go from index to index -- i.e. {source:0,target:1} goes from the
    first node in the node list to the second node. GEXF files go by ID. How we transform:
@@ -54,33 +56,46 @@ function xmlToGraph(xmlDoc)
 
 
 var tickLimit = 1; // perhaps change this to a function.
-function plotGraph(svg, nodes, edges)
+function plotGraph(scene, nodes, edges)
 {
-    var width = parseInt(svg.attr("width")), height = parseInt(svg.attr("height"));
+
+    var width = parseInt(scene.attr("width")), height = parseInt(scene.attr("height"));
   
 
-    var force = d3.layout.force()
-	.gravity(.05)
-	.distance(100)
-	.charge(-100)
-	.size([width, height]);
+    var force = d3.layout.force3d()
+    	.gravity(.05)
+	    .distance(100)
+	    .charge(-100)
+	    .size([50, 50]);
 
     force
-	.nodes(nodes)
-	.links(edges)
-	.start();
+	    .nodes(nodes)
+	    .links(edges)
+	    .start();
 
-    var link = svg.selectAll(".link")
-	.data(edges)
-	.enter().append("line")
-	.attr("class", "link");
 
-    var node = svg.selectAll(".node")
-	.data(nodes)
-	.enter().append("g")
-	.attr("class", "node")
-	.call(force.drag);
+    var link = scene.selectAll(".link")
+	    .data(edges)
+	    .enter().append("x3d:shape")
+	    .attr("class", "link")
+        .html("<indexedlineset coordindex='0 1 ' ><coordinate point='0 0 0 0 0 0' class='line'></coordinate></indexedlineset>").select(".line"); // fixi
 
+
+
+    var node = scene.selectAll(".node")
+	    .data(nodes)
+	    .enter()
+        .append("transform")
+	    .attr("class", "node");
+       // .html("<shape><appearance><material diffuseColor='black'></material></appearance><sphere radius='1.0' /></shape>");
+  
+    var sh = node.append("shape").attr("id", function(d) { return d.label + "_sh"; }) ;
+    sh.append("appearance").append("material").attr("diffuseColor",'steelblue');
+    sh.append("sphere").attr("radius='1.0'");
+
+    //.call(force.drag);
+
+        /*
     node.append("circle")
 	.attr("class", "node")
 	.attr("x", -8)
@@ -88,41 +103,50 @@ function plotGraph(svg, nodes, edges)
 	.attr("r", 8)
     .attr("fill", function(d) { return d.color; })
 	.attr("id", function(d) { return d.label });
-    
+    */
+
     node.append("text")
-	.attr("dx", 12)
-	.attr("dy", ".35em")
-	.text(function(d) { return d.label});
+	//.attr("dx", 12)
+	//.attr("dy", ".35em")
+	.text(function(d) { return d.label });
+
 
     var tick = tickLimit;
     force.on("tick", function() {
-	if (tick == tickLimit) {
-	    link.attr("x1", function(d) { return d.source.x; })
-		.attr("y1", function(d) { return d.source.y; })
-		.attr("x2", function(d) { return d.target.x; })
-		.attr("y2", function(d) { return d.target.y; });
+	    if (tick == tickLimit) {
+	        link.attr("point",
+                function(d) {
+                return x(d.target.x)+" "+y(d.target.y)+" "+z(d.target.z)+" "
+                 +x(d.source.x)+" "+y(d.source.y)+" "+z(d.source.z)});
 
-	    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-	    tick = 0;
-	}
-	else
-	    tick++;
+	        node.attr("translation", function(d) { return x(d.x)+" "+y(d.y)+" "+z(d.z) });
+	        tick = 0;
+	    }
+    	else
+	        tick++;
     });
-    
+   
 }
+
+// set up the axes
+var x = d3.scale.linear().domain([0, 100]).range([0, 10]),
+y = d3.scale.linear().domain([0, 100]).range([0, 10]),
+z = d3.scale.linear().domain([0, 100]).range([0, 10]);
 
 
 function findNode(e) {
     var name = e.target.value;
-    var node = document.getElementById(name);
+    var node = d3.select('#' + name + "_sh");
     if (node != null && document.getElementById("row_" + name) == null) {
-	node.setAttribute("fill", "red");
-	var row = d3.select("#selected").append("tr");
+	
+        node.select("material").attr("diffuseColor", "red");
+
+	    var row = d3.select("#selected").append("tr");
     
-	row.html(name + " ");
-	row.attr("id", "row_" + name)
-	row.append("a").html("x").attr("href", "#")
-            .on("click", function() { removeNode(name) });
+    	row.html(name + " ");
+    	row.attr("id", "row_" + name)
+    	row.append("a").html("x").attr("href", "#")
+             .on("click", function() { removeNode(name) });
     }
     
 }
@@ -132,8 +156,8 @@ function removeNode(name) {
   if (o != null)
    o.parentNode.removeChild(o);
 
-  var node = document.getElementById(name);
-  if (node != null) node.setAttribute("fill", "black");
+  var node = d3.select('#' + name + "_sh");
+  if (node != null) node.select("material").attr("diffuseColor", "steelblue");
 }
 
 
@@ -154,11 +178,12 @@ function handleFileSelect(evt) {
 	    
 	    var graph = xmlToGraph(xmlDoc);
 	    
-	    d3.select("#vis").selectAll("*").remove(); // remove existing vis, if necessary
-	
+	    //d3.select("#vis").selectAll("*").remove(); // remove existing vis, if necessary
+	    
+
 	    if (graph.nodes != "") {
-		cls();
-		plotGraph(d3.select("#vis"), graph.nodes, graph.edges);
+		    cls();
+		    plotGraph(d3.select("#vis"), graph.nodes, graph.edges);
 	    }
 	    else {
 		msg("Unable to load XML file.");
@@ -167,8 +192,6 @@ function handleFileSelect(evt) {
 	reader.readAsText(f);
     }
 }
-
-
 
 /*
    'Array.findIndex' may not be in all JS implementations. See:
