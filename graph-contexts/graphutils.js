4b495ca89ca94705ba57cac8f3822dfeff5272aa
Simon Jacobs <sdjacobs@uchicago.edu>
2014-08-21 15:29:17 -0500

    Initial commit for sanky

--- graph-contexts/graphutils.js
+++ graph-contexts/graphutils.js
@@ -153,6 +153,117 @@ function createTable(table, header, nodes, func) {
 }
 
 
+function getSankeyData(words, contexts) {
+    
+    var nodes = [];
+    var edges = {};
+
+    var nodeadd = function (s) { if (! (s in nodes)) nodes.push(s); };
+    
+    var edgeadd = function (n, m) {
+        var idx = [nodes.indexOf(n), nodes.indexOf(m)];
+        if (edges.hasOwnProperty(idx))
+            edges[idx] = edges[idx] + 1;
+        else
+            edges[idx] = 1;
+    }
+
+    var collapseNodes = function(ns) {
+       return ns.map(function(n) { return {"name": n}; });
+    }
+    var collapseEdges = function(es) {
+        var edgelist = [];
+        for (i in es)
+            edgelist.push({"source":i[0], "target":i[1], "value":es[i].toString()});
+        return edgelist;
+    }
+
+    for (var i in words) {
+        var w = words[i];
+        nodeadd(w);
+        for (var j in contexts[w]) {
+            var ctx = contexts[w][j];
+            var cs = ctx.split(' ');
+            for (var k = 0; k < cs.length && cs[k] != "__"; k++) {
+                nodeadd(cs[k]);
+                edgeadd(cs[k], w);
+            }
+            k++;
+            for (; k < cs.length; k++) {
+                nodeadd(cs[k]);
+                edgeadd(w, cs[k]);
+            }
+        }
+    }
+
+
+    return {"nodes":collapseNodes(nodes), "links":collapseEdges(edges)};
+}
+
+
+function createSankey(data, svg) {
+    var width = svg.attr("width"), height = svg.attr("height");
+    
+    var sankey = d3.sankey()
+        .nodeWidth(15)
+        .nodePadding(10)
+        .size([width, height]);
+
+    var path = sankey.link();
+    sankey
+      .nodes(data.nodes)
+      .links(data.links)
+      .layout(32);
+
+  var link = svg.append("g").selectAll(".link")
+      .data(energy.links)
+    .enter().append("path")
+      .attr("class", "link")
+      .attr("d", path)
+      .style("stroke-width", function(d) { return Math.max(1, d.dy); })
+      .sort(function(a, b) { return b.dy - a.dy; });
+
+  link.append("title")
+      .text(function(d) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
+
+  var node = svg.append("g").selectAll(".node")
+      .data(energy.nodes)
+    .enter().append("g")
+      .attr("class", "node")
+      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+    .call(d3.behavior.drag()
+      .origin(function(d) { return d; })
+      .on("dragstart", function() { this.parentNode.appendChild(this); })
+      .on("drag", dragmove));
+
+  node.append("rect")
+      .attr("height", function(d) { return d.dy; })
+      .attr("width", sankey.nodeWidth())
+      .style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); })
+      .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
+    .append("title")
+      .text(function(d) { return d.name + "\n" + format(d.value); });
+
+  node.append("text")
+      .attr("x", -6)
+      .attr("y", function(d) { return d.dy / 2; })
+      .attr("dy", ".35em")
+      .attr("text-anchor", "end")
+      .attr("transform", null)
+      .text(function(d) { return d.name; })
+    .filter(function(d) { return d.x < width / 2; })
+      .attr("x", 6 + sankey.nodeWidth())
+      .attr("text-anchor", "start");
+
+  function dragmove(d) {
+    d3.select(this).attr("transform", "translate(" + d.x + "," + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ")");
+    sankey.relayout();
+    link.attr("d", path);
+  }
+}
+
+
+
 /* Download SVG feature */
 
 function svgToText(svg) {
@@ -280,13 +391,16 @@ if (!Array.prototype.remove) {
       var list = Object(this);
       var length = list.length >>> 0;
       
+      var j = 0;
       for (var i = 0; i < length; i++) {
         if (i in list) {
-          if (list[i] === val)
-                list.splice(i,1)
+          if (list[i] === val) {
+            j++;
+                 list.splice(i,1)
+          }
         }
       }
-      return -1;
+      return j;
     }
   });
 }
